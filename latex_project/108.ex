5d1733c6d34710327ce5951af771bcb93eac1400
MilenaDukanac <sekapersa8@gmail.com>
2019-09-14 16:59:38 +0200

    Delete 108.ex

--- latex_project/108.ex
+++
@@ -1,9 +0,0 @@
-def calculate([], z_table), do: z_table
-def calculate([head|tail], z_table) do
-  z_table = if(Map.get(z_table, head) == nil) do
-    Map.put(z_table, head, 1)
-  else
-    Map.put(z_table, head, Map.get(z_table, head) + 1)
-  end
-  z_table = calculate(tail, z_table)
-end
